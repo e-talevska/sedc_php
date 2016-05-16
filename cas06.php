7723931ce3556beba5a6b1b9a4b335481f182e3e
e-talevska <talevska@kyos.com>
2016-05-16 21:19:10 +0200

    branch after merge

--- cas06.php
+++ cas06.php
@@ -1,36 +1,10 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-function my_name()
-{
-    echo "dejan";
-=======
 
 function my_name() {
     echo "Emilija Talevska";
->>>>>>> emilija_talevska
 }
 
 ?>
 <div>
-<<<<<<< HEAD
     <p>ova ke se prikaze na stranata</p>
 </div>
-
-
-
-
-
-=======
-    <p>Ova ke se prikaze vo stranata</p>
-</div>
->>>>>>> emilija_talevska
-=======
-function my_name(){
-    echo "jovce";
-}
-?>
-<div>
-    <p>Ova ke se prikaze vo stranata</p>
-</div>
->>>>>>> 1a9e7f7b28ed399d446050ef5583e077c832b001
