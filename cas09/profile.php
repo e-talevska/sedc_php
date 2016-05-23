bb5dc0b5f0d9c708e5edc24124f2ef65b3eb1b35
Dime Levov <levov.dime@gmail.com>
2016-05-23 21:06:47 +0200

    working with files

--- cas09/profile.php
+++ cas09/profile.php
@@ -5,8 +5,55 @@ if(isset($_SESSION['id'])===false){
     exit;
 
 }
+var_dump(__FILE__);
+$errors=array();
 if(isset($_POST['upload'])){
     var_dump($_FILES);
+    $file_errors=array(
+        UPLOAD_ERR_INI_SIZE => 'file larger then upload_max_filesize',
+        UPLOAD_ERR_FORM_SIZE => 'post expands post_max_size',
+        UPLOAD_ERR_PARTIAL => 'file not uploaded completely, please try again',
+        UPLOAD_ERR_NO_FILE => 'please choose a file to upload',
+        UPLOAD_ERR_NO_TMP_DIR => 'no temporary directory found',
+        UPLOAD_ERR_CANT_WRITE => 'cannot write to disk',
+        UPLOAD_ERR_EXTENSION => 'file upload stopped by extension'
+    );
+
+    if($_FILES['profile_picture']['error']==UPLOAD_ERR_OK){
+        if($_FILES['profile_picture']['type']=="image/jpeg" || $_FILES['profile_picture']['type']=="image/gif"||$_FILES['profile_picture']['type']=="image/png" ){
+            if((($_FILES['profile_picture']['size'])/1024)<300){
+                $name=$_FILES['profile_picture']['name'];
+                if(file_exists("uploads/").$name){
+                    $pathinfo=pathinfo("uploads/".$name);
+                    $name=$pathinfo['filename'].time().".".$pathinfo['extension'];
+                }
+                if(move_uploaded_file($_FILES['profile_picture']['tmp_name'],"uploads/".$name)){
+                    $errors['profile_picture']="file uploads was successful";
+
+
+                }
+
+                else{
+                    $errors['profile_picture']="file uploads was not successful";
+                }
+
+            }
+            else{
+
+               $errors['profile_picture']="the uploaded file is too big";
+            }
+
+
+    }
+        else{
+            $errors['profile_picture']="the uploaded file is not a picture";
+        }
+
+    }
+    else{
+        $error=$_FILES['profile_picture']['error'];
+        $errors['profile_picture']=$file_errors[$error];
+    }
 }
 
 ?>
@@ -23,7 +70,10 @@ if(isset($_POST['upload'])){
             <p>
                 <label> upload profile picture</label>
                 <input type="file" name="profile_picture">
-                <span></span>
+                <br>
+                <span>
+                    <?php if(isset($errors['profile_picture'])){echo $errors['profile_picture'];} ?>
+                </span>
             </p>
             <p>
                 <button type="submit" name="upload">upload</button>
