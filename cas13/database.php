f46d312cd88fcd43bc30db933c92e177113aaad0
Jordan Jordanovski <jordanovskijordan@hotmail.com>
2016-06-01 21:33:09 +0200

    php working with mysql

---
+++ cas13/database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Code
+ * Date: 01.06.2016
+ * Time: 17:58
+ */
+
+function connect_to_db(){
+    $db_connect = mysqli_connect("localhost", "root", "", "widget_corp");
+    if (mysqli_connect_errno()) {//ovde ke vleze samo ako ima error(>0)
+        die("Database connection error:" . mysqli_connect_error());
+    }
+    return $db_connect;
+}
+function disconnect_from_db($db_connect){
+//5.zatvori konekcija so mysql
+    mysqli_close($db_connect);
+}
+
---
+++ cas13/subjects_delete.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Code
+ * Date: 01.06.2016
+ * Time: 20:38
+ */
+if(isset($_GET["id"])) {
+    require_once("database.php");
+    $db_connect = connect_to_db();
+
+    $query = "DELETE FROM subjects ";
+    $query .= "WHERE id = " . $_GET["id"];
+    $result = mysqli_query($db_connect, $query);
+        if($result){
+            header("Location: subjects_list.php");
+            exit;
+        }
+
+    disconnect_from_db($db_connect);
+}else {
+    header("Location: subjects_list.php");
+    exit;
+}
